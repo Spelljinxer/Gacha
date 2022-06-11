08c2ef4afd13f8f99b9c845d43977b68d28c073c
Spelljinxer <spelljinxer@DESKTOP-5LQ8J1I.localdomain>
2022-06-12 02:16:46 +0800

    commeting

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ standard_five_stars = [
 ]
 
 
-    
-
 def exec_gacha(banner, pull_num):
     pull_counter = 0
     soft_pity = 0
@@ -18,23 +16,28 @@ def exec_gacha(banner, pull_num):
 
     if(banner == "s" or "l"):
         soft_pity = 75
-        hard_pity = 90
     if(banner == "w"):
         soft_pity = 65
-        hard_pity = 80
+        
 
-
+    hard_pity = soft_pity + 15
 
     while(pull_counter < pull_num):
         gacha_rate = random.uniform(0.0000, 1.000)
-        #print("gacha_rate: " + str(gacha_rate))
+        
+
+        #going to need to fix these to correspond with the actual genshin rates (math dansgame):
+            #https://www.reddit.com/r/Genshin_Impact/comments/jo9d9d/the_5_rate_is_not_uniform_06_there_is_a_soft_pity/
+            #https://genshin-impact.fandom.com/wiki/Cumulative_Probability 
         if(gacha_rate <= 0.0006):
             print("Congratulations! You got: ", random.choice(standard_five_stars))
             break
+
         pull_counter += 1
         if(pull_counter == hard_pity):
             print("You've reached Hard Pity. You got: ", random.choice(standard_five_stars))
             break
+        
     
     pass
 
