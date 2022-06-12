aaeaccfe453cfb42289fb208b3bccd5084742c45
Spelljinxer <spelljinxer@DESKTOP-5LQ8J1I.localdomain>
2022-06-12 16:39:23 +0800

    error checking + refactor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, random
+import sys, random, time
 
 standard_five_stars = [
     "Diluc",
@@ -8,21 +8,42 @@ standard_five_stars = [
     "Qiqi"
 ]
 
+five_star_weapons = [
+    "Amos' Bow",
+    "Skyward Harp",
+    "Lost Prayer to the Sacred Winds",
+    "Skyward Atlas",
+    "Skyward Pride",
+    "Wolf's Gravestone",
+    "Skyward Spine",
+    "Aquila Favonia",
+    "Skyward Blade"
+]
 
-def exec_gacha(banner, pull_num):
+limited_banner = [
+
+]
+
+def exec_gacha(charname, banner, pull_num):
     pull_counter = 0
     soft_pity = 0
     hard_pity = 0
-
-    if(banner == "s" or "l"):
+    pull_banner = []
+    if(banner == "s"):
         soft_pity = 75
+        pull_banner = standard_five_stars
+    if(banner == "l"):
+        soft_pity = 75
+        pull_banner = limited_banner
     if(banner == "w"):
         soft_pity = 65
-        
+        pull_banner = five_star_weapons
 
     hard_pity = soft_pity + 15
 
     while(pull_counter < pull_num):
+        time.sleep(0.85)
+        print("Pulling...")
         gacha_rate = random.uniform(0.0000, 1.000)
         
 
@@ -30,26 +51,39 @@ def exec_gacha(banner, pull_num):
             #https://www.reddit.com/r/Genshin_Impact/comments/jo9d9d/the_5_rate_is_not_uniform_06_there_is_a_soft_pity/
             #https://genshin-impact.fandom.com/wiki/Cumulative_Probability 
         if(gacha_rate <= 0.0006):
-            print("Congratulations! You got: ", random.choice(standard_five_stars))
+            print("Congratulations! You got: ", random.choice(pull_banner))
             break
 
         pull_counter += 1
         if(pull_counter == hard_pity):
-            print("You've reached Hard Pity. You got: ", random.choice(standard_five_stars))
+            print("You've reached Hard Pity. You got: ", random.choice(pull_banner))
             break
         
     
-    pass
+    pass #is this really needed??
 
+def print_usage():
+    return("Usage: python3 main.py [-s, -l charname, -w]  <number of rolls>\n\n(-s for standard, -l for limited, -w for weapon) ")
      
 def main():
-    if(len(sys.argv) != 3):
-        print("Usage: python3 gacha.py [-s, -l, -w]  <number of rolls>\n\n(-s for standard, -l for limited, -w for weapon) ")
-        sys.exit(1)
+    try:
+        if (not len(sys.argv) >= 3 or len(sys.argv) > 4):
+            print_usage()
+            sys.exit(1)
+        charname = ""
+        pull_num = 0
+        if(sys.argv[1] == "-l"):
+            charname = sys.argv[2]
+            pull_num = int(sys.argv[3])
+        else:
+            pull_num = int(sys.argv[2])
+        banner = sys.argv[1][1]
+        print(exec_gacha(charname, banner, pull_num))
 
-    pull_num = int(sys.argv[2])
-    banner = sys.argv[1][1]
-    print(exec_gacha(banner, pull_num))
+        sys.exit(0)
+    except:
+        print_usage()
+        sys.exit(1)
     
     #print("number of arguments : ", len(sys.argv))
     #print("arguments : ", str(sys.argv))
